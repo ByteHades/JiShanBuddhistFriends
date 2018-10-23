14386df6398f840b40340130f1d5b7a99a95f02d
任环山 <17610675350@139.com>
2018-10-23 13:48:58 +0800

    初步实现

---
+++ JiShanBuddhistFriends/Classes/BFDefine.swift
@@ -0,0 +1,14 @@
+//
+//  BFDefine.swift
+//  JiShanBuddhistFriends
+//
+//  Created by Simon on 2018/10/23.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let DefineScreenWidth = UIScreen.main.bounds.width
+let DefineScreenHeight = UIScreen.main.bounds.height
+

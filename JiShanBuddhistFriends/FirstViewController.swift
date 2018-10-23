9a16400ae8e0d4736a89d34089d39d7a3e99531a
任环山 <17610675350@139.com>
2018-10-23 11:44:51 +0800

    创建项目文件

---
+++ JiShanBuddhistFriends/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JiShanBuddhistFriends
+//
+//  Created by Simon on 2018/10/23.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ JiShanBuddhistFriends/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  JiShanBuddhistFriends
+//
+//  Created by Simon on 2018/10/23.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
